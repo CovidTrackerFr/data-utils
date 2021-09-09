95b85c3f0b4c538dff14f10a546c3c20ab8228fa
rozierguillaume <guillaume.rozier73@gmail.com>
2021-09-09 13:59:19 +0200

    new data

---
+++ edutracker/src_situation_academique.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+import json
+
+CWD = os.getcwd()
+
+def get_data():
+    return pd.read_csv(CWD + "/data/fr-en-situation_academique_covid.csv", sep=";")
+
+def df_to_json(df):
+    academies = list(df.academie.unique())
+
+    dict_json = {'academies': academies}
+
+    for academie in academies:
+        df_academie = df[df.academie == academie]
+        dict_academie = {}
+        for col in df_academie.columns:
+            if col not in ["academie"]:
+                dict_academie[col] = df_academie[col].fillna(0).to_list()
+        dict_json[academie] = dict_academie
+    return dict_json
+
+def export_json(data_json):
+    with open(CWD + '/data/' + 'fr-en-situation_academique_covid.json', 'w', encoding='utf-8') as f:
+        json.dump(data_json, f, ensure_ascii=False, indent=4)
+
+
+def main():
+    df = get_data()
+    data_json = df_to_json(df)
+    export_json(data_json)
+
+if __name__ == "__main__":
+    main()
--- edutracker/src_situation_nationale.py
+++ edutracker/src_situation_nationale.py
@@ -5,7 +5,7 @@ import json
 CWD = os.getcwd()
 
 def get_data():
-    return pd.read_excel(CWD + "/data/covid_menjs_France.xlsx")
+    return pd.read_csv(CWD + "/data/fr-en-situation_nationale_covid.csv", sep=";")
 
 def df_to_json(df):
     dict_json = {}
@@ -14,7 +14,7 @@ def df_to_json(df):
     return dict_json
 
 def export_json(data_json):
-    with open(CWD + '/data/' + 'covid_menjs_France.json', 'w', encoding='utf-8') as f:
+    with open(CWD + '/data/' + 'fr-en-situation_nationale_covid.json', 'w', encoding='utf-8') as f:
         json.dump(data_json, f, ensure_ascii=False, indent=4)
 
 
